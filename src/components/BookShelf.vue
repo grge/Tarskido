cb9b4014974d4aacfd24cfa953a898db14f313bf
George Dickeson <George.Dickeson@ekistica.com.au>
2020-06-20 13:04:22 +0930

    Style improvements to Bookshelf

--- src/components/BookShelf.vue
+++ src/components/BookShelf.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <div class='bookgrid'>
         <div class='bookgridbook' :key='id' v-for='(book, id) in books'>
-            <p>{{ book.title }}</p>
-            <p>by {{ book.author }}</p>
+            <p class='bookshelf-title'>{{ book.title }}</p>
+            <p class='bookshelf-author'>by {{ book.author }}</p>
         </div>
     </div> 
     <a v-if="$store.state.editMode" class='editlink newbooklink'  @click="$store.commit('createNewBook')">Create new book</a>
@@ -22,16 +22,29 @@ export default {
 <style scoped lang="stylus">
 .bookgrid
   display grid
-  width: 450px
+  width: 600px
   margin 1.5em auto
-  grid-template-columns 150px 150px 150px
+  grid-template-columns 150px 150px 150px 150px
   background white
   border-top 1px solid #aaaaaa
   border-bottom 1px solid #aaaaaa
 
 .bookgridbook
   text-align center
-  width 135px
-  margin 0 auto
+  margin 1.5em auto
+  padding 0 1em
+
+.bookshelf-title::before
+  content "🕮"
+  display block
+
+.bookshelf-title
+  font-size 11pt
+  margin 0px
+
+.bookshelf-author
+  margin 0px
+  font-size 9pt
+  font-style italic
 
 </style>
