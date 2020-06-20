f0c04152c9dbea61a7395c196f38d72dd248ea21
George Dickeson <george.dickeson@gmail.com>
2020-06-20 15:28:04 +0930

    Add hamburger menu

---
+++ src/components/HamburgerMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hamburger-menu">
+      <img v-if='dark' class="hamburger-menu-button" src="hamburger_icon_dark.png" />
+      <img v-if='!dark' class="hamburger-menu-button" src="hamburger_icon_light.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HamburgerMenu',
+  props: {
+      dark: Boolean
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.hamburger-menu-button
+   position absolute
+   right 0
+   top 0
+   margin 15px
+   width 30px
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="home">
+      <HamburgerMenu :dark='false'/>
       <h1>Tarskido</h1>
       <BookShelf :books='$store.state.books'/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import BookShelf from '@/components/BookShelf.vue'
+import HamburgerMenu from '@/components/HamburgerMenu.vue'
 
 export default {
   name: 'Home',
   components: {
-    BookShelf
+    BookShelf,
+    HamburgerMenu
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang='stylus'>
+
+h1
+ margin-top 2em
+ font-size 41pt
+ font-weight normal
+ font-family sans
+
+h1::before
+ content "◉"
+ margin-right 0.5em
 </style>
